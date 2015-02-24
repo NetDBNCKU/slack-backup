86c5689a52f322cdf9c1bc6aee6fd9c921249172
Frank Yang <yangpoan@gmail.com>
2015-02-24 18:22:24 +0800

    add private_channels & private_message

--- backup-mandrill.py
+++ backup-mandrill.py
@@ -11,6 +11,7 @@ class Backup:
         self.token = str()
         self.user_dict = dict()
         self.channel_list = list()
+        self.private_channel_list = list()
         self.send_data = list()
         self.mailgun_key = str()
         self.mandrill_key = str()
@@ -41,6 +42,20 @@ class Backup:
             self.channel_list.append(channel_dict)
 #        print (self.channel_list)
 #        print(json.dumps(obj,sort_keys=True, indent=4, separators=(',', ': ')))
+
+    def get_private_channels(self):
+        req_url = "https://slack.com/api/groups.list?token=" + self.token
+        print(req_url)
+        response = urllib.request.urlopen(req_url)
+        str_response = response.read().decode('utf-8')
+        obj = json.loads(str_response)
+        for private_channel in obj['groups']:
+            private_channel_dict = dict()
+            private_channel_dict['id'] = private_channel['id']
+            private_channel_dict['name'] = private_channel['name']
+            self.private_channel_list.append(private_channel_dict)
+#        print (self.private_channel_list)
+#        print (json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': ')))
 
     def get_users(self):
         req_url = "https://slack.com/api/users.list?token=" + self.token
@@ -92,6 +107,48 @@ class Backup:
                 else:
                     break
             self.send_data.append(channel_data)
+
+
+    def get_private_message(self):
+        self.oldest = "0"
+        self.latestTime = time()
+        for channel in self.private_channel_list:
+            channel_data = dict()
+            channel_data['channel_name'] = channel['name']
+            channel_data['messages'] = list()
+            while True:
+                req_url = "https://slack.com/api/groups.history?token=" + self.token\
+                                + "&channel=" + channel['id']\
+                                + "&oldest=" + str(self.oldest)\
+                                + "&latest=" + str(self.latestTime)\
+                                + "&count=1000"
+                print (req_url)
+                response = urllib.request.urlopen(req_url)
+                str_response = response.read().decode('utf-8')
+                obj = json.loads(str_response)
+                for message in obj['messages']:
+#                    print (json.dumps(message,sort_keys=True, indent=4, separators=(',', ': ')))
+                    message_dict = dict()
+                    try:
+                        message_dict['user'] = self.user_dict[message['user']]
+                    except:
+                        if "username" in list(message.keys()):
+                            message_dict['user'] = message['username']
+                        elif "bot_id" in list(message.keys()):
+                            message_dict['user'] = message['bot_id']
+                        else:
+                            message_dict['user'] = self.user_dict[message['comment']['user']] # Comment post
+                    message_dict['text'] = message['text']
+                    message_dict['ts'] = message['ts']
+                    channel_data['messages'].append(message_dict)
+#                print (json.dumps(channel_data['messages'],sort_keys=True, indent=4, separators=(',', ': ')))
+                if obj['has_more']:
+                    latest = obj['messages'][0]['ts']
+                else:
+                    break
+#            print (channel_data)
+            self.send_data.append(channel_data)
+
     def set_latest_backup_time(self):
         file = open(self.now_path + '/latest_backup_time.txt','w')
         file.write(str(self.latestTime))
@@ -118,7 +175,7 @@ class Backup:
             "https://api.mailgun.net/v2/sandbox3b110172ed844490b95b97eb9ef9c178.mailgun.org/messages",
             auth=("api", self.mailgun_key),
             data={"from": "Mailgun Sandbox <postmaster@sandbox3b110172ed844490b95b97eb9ef9c178.mailgun.org>",
-                  "to": ["arvin0731@gmail.com","ktchuang@gmail.com"],
+                  "to": ["arvin0731@gmail.com","ktchuang@gmail.com","yangpoan@gmail.com"],
                   "subject": "Slack Backup",
                   "text": json.dumps(self.send_data,sort_keys=True, indent=4, separators=(',', ': '),ensure_ascii=False) })
             #"ktchuang@gmail.com"
@@ -146,16 +203,23 @@ class Backup:
 
 
 def main():
-   b = Backup()
-   b.get_pwd()
-   b.get_token()
-   b.get_channels()
-   b.get_users()
-   b.get_message()
-   b.get_mailgun_key()
-   b.send()
-   b.get_mandrill_key()
-   b.send_mail('template-1', ["arvin0731@gmail.com"])
+    b = Backup()
+    b.get_pwd()
+    b.get_token()
+    b.get_channels()
+    b.get_users()
+    b.get_message()
+    b.get_mailgun_key()
+    b.send()
+#   b.get_mandrill_key()
+#   b.send_mail('template-1', ["arvin0731@gmail.com"])
 
+    b_private = Backup()
+    b_private.get_pwd()
+    b_private.get_token()
+    b_private.get_private_channels()
+    b_private.get_users()
+    b_private.get_private_message()
+    b_private.send()
 if __name__ == '__main__':
     main()
